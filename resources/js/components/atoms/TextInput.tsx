6bb823f4959bed3e4ecbdb513b8ba29bb0709e4a
cdevlegend <devsecretstar@gmail.com>
2023-09-06 22:51:25 +0900

    TextInput

--- resources/js/components/atoms/TextInput.tsx
+++ resources/js/components/atoms/TextInput.tsx
@@ -7,7 +7,6 @@ interface ITextInput extends InputHTMLAttributes<HTMLInputElement> {
     required?: boolean;
     startImg ?: string;
 }
-
 export const TextInput: (props: ITextInput) => ReactElement  = ({name, type,  ...rest }) => {
     return (
         <div className={`${rest.className ?? ''}`}>
@@ -15,6 +14,7 @@ export const TextInput: (props: ITextInput) => ReactElement  = ({name, type,  ..
             <div className={`rounded-10 h-10 my-3 `}>
                 <img src={rest.startImg?'assets/img/icon/p-2.svg':''} alt=""  className="absolute h-10 align-middle "/>
                 <input type={type} name={name} {...rest} placeholder={`Enter your ${rest.label}`} className="rounded-10 w-full"/>
+                <div></div>
             </div>
         </div>
     )
